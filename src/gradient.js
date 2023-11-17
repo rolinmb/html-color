d6a7fe04653555dc7f1d59e33dc4a089077c303c
Rolin Blake <rolinmblake@gmail.com>
2023-11-17 11:31:40 -0600

    refactoring repeated code into index.html

--- src/gradient.js
+++ src/gradient.js
@@ -1,15 +1,3 @@
-var tLast;
-const interval = 1000;
-const letters = "0123456789ABCDEF";
-
-function getRandomColor() {
-  let clr = "#";
-  for (let i = 0; i < 6; i++) {
-    clr += letters[Math.floor(Math.random()*16)];
-  }
-  return clr;
-}
-
 function updateGradient() {
   const grad = "linear-gradient("+getRandomColor()+","+getRandomColor()+")";
   document.documentElement.style.background = grad;
--- src/simple.js
+++ src/simple.js
@@ -1,18 +1,7 @@
-var tLast;
-const interval = 1000;
-const letters = "0123456789ABCDEF";
 const canvas = document.getElementById("clr-canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-
-function getRandomColor() {
-  let clr = "#";
-  for (let i = 0; i < 6; i++) {
-    clr += letters[Math.floor(Math.random()*16)];
-  }
-  return clr;
-}
 
 function updateCanvasColor() {
   ctx.fillStyle = getRandomColor();
